68b6fd3c9c2e23fd804e736beaefe096715d9d8a
BALUSANGEM <vishnubalu.sangem@gmail.com>
2018-12-25 02:40:53 +0530

    Added extension function to list of kingdoms to getKingdomsHavingMaximumAllies

---
+++ src/UtilityExtensions.kt
@@ -0,0 +1,14 @@
+fun List<Kingdom>.getKingdomsHavingMaximumAllies(): List<Kingdom> {
+    val listOfKingdomsWithMaxAllies = ArrayList<Kingdom>()
+    var maxAllies = 0
+    forEach { kingdom ->
+        if (maxAllies == kingdom.allies.size) {
+            listOfKingdomsWithMaxAllies.add(kingdom)
+        } else if (maxAllies < kingdom.allies.size) {
+            maxAllies = kingdom.allies.size
+            listOfKingdomsWithMaxAllies.clear()
+            listOfKingdomsWithMaxAllies.add(kingdom)
+        }
+    }
+    return listOfKingdomsWithMaxAllies
+}
