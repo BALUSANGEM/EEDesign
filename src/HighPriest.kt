87824469a2a4f58a0c3605a81fcce0aa2125b1e5
BALUSANGEM <vishnubalu.sangem@gmail.com>
2018-12-25 02:39:41 +0530

    High priest can pick messages and send them to respective kingdoms

--- src/HighPriest.kt
+++ src/HighPriest.kt
@@ -1,3 +1,27 @@
+import java.util.*
+
 class HighPriest {
 
+    private val pickedMessagesList = ArrayList<Message>()
+    fun pickMessagesFromBallet(ballot: Ballot) {
+        pickedMessagesList.clear()
+
+        val allMessages = ballot.messages
+        while (true) {
+            val message = allMessages[Random().nextInt(allMessages.size)]
+            if (!pickedMessagesList.contains(message)) {
+                pickedMessagesList.add(message)
+            }
+            if (pickedMessagesList.size >= 6) {
+                break
+            }
+        }
+
+    }
+
+    fun sendMessagesToPickedKingdoms() {
+        pickedMessagesList.forEach { message ->
+            message.receiver.receiveMessage(message)
+        }
+    }
 }
