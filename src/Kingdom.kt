5995b9cc01e996b59038db4c0c38bab568de1303
BALUSANGEM <vishnubalu.sangem@gmail.com>
2018-12-25 02:10:22 +0530

    Kingdom can now participate in voting and can compete

--- src/Kingdom.kt
+++ src/Kingdom.kt
@@ -1,5 +1,12 @@
 class Kingdom(val name: String, val emblem: String, val rulerName: String = "") {
 
+    var isCompetingToBeRuler = false
+        set(value) {
+            allies.clear()
+            alligienceTo = null
+            field = value
+        }
+
     val allies = ArrayList<Kingdom>()
     var alligienceTo: Kingdom? = null
 
@@ -7,10 +14,17 @@ class Kingdom(val name: String, val emblem: String, val rulerName: String = "") 
         message.receiver.receiveMessage(message)
     }
 
-    private fun receiveMessage(message: Message) {
-        if (MessageMatchingAlgo().isMessageMatchingWithEmblem(message.message, emblem)) {
-            alligienceTo = message.sender
-            message.sender.onAllegianceMadeBy(this)
+    fun sendMessageAsVote(ballot: IBallot, receiverKingdom: Kingdom) {
+        val randomMessage = MessagesTable().getRandomMessage()
+        ballot.addMessageAsVote(Message(this, receiverKingdom, randomMessage))
+    }
+
+    fun receiveMessage(message: Message) {
+        if (AllyDecider().isAlly(MessageMatchingAlgo(message.message, emblem))) {
+            if (alligienceTo == null && !isCompetingToBeRuler) {
+                alligienceTo = message.sender
+                message.sender.onAllegianceMadeBy(this)
+            }
         }
     }
 
