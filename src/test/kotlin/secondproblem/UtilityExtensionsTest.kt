bdde9923cd2c6d726db62f9312fb629f7c6fc19e
BALUSANGEM <vishnubalu.sangem@gmail.com>
2018-12-25 04:34:28 +0530

    Added tests to Utility extensions

---
+++ src/test/kotlin/secondproblem/UtilityExtensionsTest.kt
@@ -0,0 +1,29 @@
+package secondproblem
+
+import org.junit.Test
+import Kingdom
+import org.junit.Assert
+
+class UtilityExtensionsTest {
+
+    @Test
+    fun whenCalledWithThisListShouldReturnExpectedOutput() {
+        val listOfKingdom = listOf(
+                Kingdom("first", "emblem1", "ruler1"),
+                Kingdom("second", "emblem2", "ruler2"),
+                Kingdom("third", "emblem3", "ruler3"),
+                Kingdom("fourth", "emble4", "ruler4"),
+                Kingdom("fifth", "emblem5", "ruler5"),
+                Kingdom("sixth", "emblem6", "ruler6")
+        )
+
+        listOfKingdom[0].allies.add(listOfKingdom[1])
+        listOfKingdom[2].allies.add(listOfKingdom[3])
+        listOfKingdom[4].allies.add(listOfKingdom[5])
+
+        Assert.assertArrayEquals(
+                arrayOf(listOfKingdom[0], listOfKingdom[2], listOfKingdom[4]),
+                listOfKingdom.getKingdomsHavingMaximumAllies().toTypedArray())
+
+    }
+}
